306ec387eaec27bb00cc72d60f4258f0738fffe7
Victor Sued <visued@gmail.com>
2020-01-04 23:22:19 -0300

    Diretivas: ngFor

---
+++ src/app/diretiva-ngfor/diretiva-ngfor.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngfor',
+  templateUrl: './diretiva-ngfor.component.html',
+  styleUrls: ['./diretiva-ngfor.component.css']
+})
+export class DiretivaNgforComponent implements OnInit {
+
+  cursos: string[] = ['C#', 'Python', 'Javascript', 'Typescript'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
